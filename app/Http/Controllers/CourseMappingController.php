c0f63f5743a8be6586bfe30fdd65c3c8442843e2
= <=>
2022-10-28 09:16:43 +0530

    Unkonwn Change

--- app/Http/Controllers/CourseMappingController.php
+++ app/Http/Controllers/CourseMappingController.php
@@ -192,7 +192,7 @@ class CourseMappingController extends Controller
         ->leftJoin('courses','categories.id','=','courses.category_id')
         ->leftJoin('qcategories','courses.id','=','qcategories.course_id')
         ->leftJoin('qtopics','qcategories.id','=','qtopics.qcategory_id')
-        ->where('courses.category_id','=',$request->id)
+        ->where('courses.category_id','=',2)
         ->get();
         dd($drive->count());
 
