4f6051c2b0ce6adf02395d61e7668fc1c445e6b8
shafikcmt <mdshafiqulislam822@gmail.com>
2022-12-10 14:56:18 +0530

    Responsive menu working

--- resources/views/includes/student-sidebar.blade.php
+++ resources/views/includes/student-sidebar.blade.php
@@ -9,6 +9,7 @@
                         <!-- <header class="header">
                             <div class="header-block header-block-collapse d-lg-none d-xl-none">
                                 <button class="collapse-btn" id="sidebar-collapse-btn">
+                                    <input type="checkbox" id="check">
                                     <i class="fa fa-bars"></i>
                                 </button>
                             </div>
